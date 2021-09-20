3f4fe2fc53f209f8d70cac6393a1888261f62c53
willhwj <williamhuwj@gmail.com>
2021-09-20 22:19:23 +0800

    added a function to cleanse hotel name for better matching across data files

--- data.js
+++ data.js
@@ -1,18 +1,34 @@
-// extract raw data from csv file into an array of hotel names
-
+// a function to extract raw data from csv file into an array of hotel names
 async function fromCSV(url) {
     let response = await axios.get(url);
     let output = await csv().fromString(response.data);
     return output;
 }
 
-// let staycayHotelsJson = async() => {
-//     let output = await fromCSV('data-source/staycay-hotels.csv');
-//     return output;
-// };
+// a function to cleanse hotel name for better matching - remove the following: Singapore, dash(-), comma(,), resorts world sentosa, convert all to small letters.
+function cleanseName(rawHotelName) {
+    let cleanName = rawHotelName.toLowerCase();
 
-// extract raw data from XML and create an array of objects, each object contains essential info of 1 hotel
+    let regExp1 = new RegExp(/(singapore|sentosa|resorts world sentosa|the|hotels*|collections*[\w\s]*|autograph|pte ltd)/, 'g');
+    cleanName = cleanName.replace(regExp1, '');
 
+    let regExp2 = new RegExp(/ (by)[\s][\w\s]+/, 'g');
+    cleanName = cleanName.replace(regExp2, ' ');
+
+    let regExp3 = new RegExp(/[\s-–,'’@s!().]+/, 'g');
+    cleanName = cleanName.replace(regExp3, '');
+
+    return cleanName;
+}
+
+let str1 = `Resorts World Sentosa – Crockfords Tower`;
+let newName1 = cleanseName(str1);
+let str2 = `Crockfords Tower`;
+let newName2 = cleanseName(str2);
+console.log(newName1);
+console.log(newName2);
+
+// a function to extract raw data from XML list of hotels by STB and create an array of objects, each object contains essential info of 1 hotel
 async function fromXML(url) {
 
     let response = await axios.get(url, { responseType: 'document' });
@@ -22,7 +38,7 @@ async function fromXML(url) {
     // exclude the first item of the array, as it's not an element of hotel data
     xmlHotelList.shift();
 
-    let hotelList = [];
+    let stbHotelList = [];
     for (let hotel of xmlHotelList) {
         // create a json object to store all SimpleData elements of 1 hotel
         let oneHotel = {};
@@ -41,49 +57,52 @@ async function fromXML(url) {
         oneHotel.COORDINATES = coordinatesLngLat;
 
         // add the hotel object to hotelList array
-        hotelList.push(oneHotel);
+        stbHotelList.push(oneHotel);
     }
-    return hotelList;
+    return stbHotelList;
 }
 
-// get coordinates for stay-home-notice hotels from hotelList array
-// create an array of stay-home-notice hotels not matched
-async function getShnHotelList() {
-    let shnHotelsRaw = await fromCSV('data-source/SHN-hotels.csv');
-    let hotelList = await fromXML('data-source/hotel-locations.kml');
-    let unmatchedShnHotels = [];
-    let shnHotelList = [];
+// a function to get coordinates for hotels based on hotelList array from XML file provided by STB 
+// also create an array of stay-home-notice hotels not matched
+// return an array of matched hotels
+async function getCoordinates(targetURL) {
+    let hotelsRaw = await fromCSV(targetURL);
+    let stbHotelList = await fromXML('data-source/hotel-locations.kml');
+    let unmatchedHotels = [];
+    let hotelList = [];
 
-    for (let s of shnHotelsRaw) {
+    for (let s of hotelsRaw) {
         let matched = false;
         let currentName = s['Hotel Name'];
         if (currentName != '') {
-            for (let h of hotelList) {
-                if (currentName === h.NAME) {
+            for (let h of stbHotelList) {
+                let cleanTargetName = cleanseName(s['Hotel Name']);
+                let cleanStbName = cleanseName(h.NAME);
+
+                if (cleanTargetName === cleanStbName || cleanTargetName.includes(cleanStbName) || cleanStbName.includes(cleanTargetName)) {
                     matched = true;
                     let matchedHotel = {};
                     matchedHotel.NAME = h.NAME;
                     matchedHotel.COORDINATES = h.COORDINATES;
                     matchedHotel.POSTALCODE = h.POSTALCODE;
                     matchedHotel.ADDRESS = h.ADDRESS;
-                    shnHotelList.push(matchedHotel);
+                    hotelList.push(matchedHotel);
                     break;
                 };
             };
             if (!matched) {
-                unmatchedShnHotels.push(s);
+                unmatchedHotels.push(s);
             }
         }
     }
-
-    console.log('after for loop, shnHotelList is ', shnHotelList);
-    console.log('unmatchedShnHotels is ', unmatchedShnHotels);
+    console.log('after for loop, hotelList is ', hotelList);
+    console.log('unmatchedHotels is ', unmatchedHotels);
+    return hotelList;
 };
 
-// getShnHotelList();
+getCoordinates('data-source/SHN-hotels.csv');
 
-// get coordinates for staycation hotels from hotelList array
-
+getCoordinates('data-source/staycay-hotels.csv');
 
 // get coordinates from FourSquare API for covid clusters
 
